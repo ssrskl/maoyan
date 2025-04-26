cdcfff527441d1b04650a79e5827d833fbb472ad
ssrskl <1071352028@qq.com>
2025-04-26 10:43:41 +0800

    更新 package.json 和 pnpm-lock.yaml，添加 appwrite 和 ofetch 依赖，版本分别为 17.0.2 和 1.4.1。

---
+++ src/lib/fetch.ts
@@ -0,0 +1,17 @@
+import { $fetch } from 'ofetch'
+export const myfetch = $fetch.create({
+    baseURL: 'https://api.example.com',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+    },
+    onRequest({ request }) {
+        console.log('Making request to:', request.toString)
+    },
+    onResponse({ response }) {
+        console.log('Received response:', response.status)
+    },
+    onResponseError({ response }) {
+        console.error('Error response:', response.status)
+    }
+})
